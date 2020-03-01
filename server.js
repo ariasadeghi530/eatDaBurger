658c60a7941508e3a93e86333c5f0e2075bd937c
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-01 12:37:04 -0800

    fixed post route

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static(join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'jsx');
+
 
 app.use(require('./routes'));
 
