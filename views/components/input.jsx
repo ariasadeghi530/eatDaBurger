bc3ee334d2326dffc59b27b85ad1e91f41580b11
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-01 12:12:46 -0800

    added jquery

--- views/components/input.jsx
+++ views/components/input.jsx
@@ -5,11 +5,11 @@ function Input() {
     <div className="col-md-4 mt-3">
       <form>
         <div class="form-group">
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          <label for="exampleFormControlTextarea1">Enter a burger you want to eat and press submit!</label>
+          <textarea class="form-control" id="textInput" rows="3"></textarea>
+          <label htmlFor="exampleFormControlTextarea1">Enter a burger you want to eat and press submit!</label>
         </div>
         <div className="text-center">
-          <button type="button" class="btn btn-primary center">Submit</button>
+          <button type="button" class="btn btn-primary center" id="submit">Submit</button>
         </div>
       </form >
     </div>
