e314adbec739e1b39f7cbc04018e46328257a812
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-29 22:10:39 -0800

    dotenv changes

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = createConnection({
   host: process.env.DB_HOST,
   port: 3306,
   user: process.env.DB_USER,
-  password: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'burgers_db'
 })
 
