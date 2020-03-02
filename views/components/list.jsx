48e30ac106bd074a56c655dca514f39f2293e00b
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-02 11:40:07 -0800

    added headers

--- views/components/list.jsx
+++ views/components/list.jsx
@@ -4,7 +4,7 @@ const React = require('react');
 function ListFalse() {
   return (
     <div className="col-md-4 col-sm-12 mt-3">
-      <h6>Click on a burger to devour it!</h6>
+      <h6 id="notEaten">Click on a burger to devour it!</h6>
       <ul class="list-group" id="unEaten">
         {/* <a><li class="list-group-item">Cras justo odio</li></a>  */}
       </ul>
