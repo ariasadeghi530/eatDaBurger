32a8b51f7a1402085a725f1bd327ef12736c02e1
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-02 11:27:39 -0800

    added headings for list

--- views/components/listEat.jsx
+++ views/components/listEat.jsx
@@ -4,6 +4,7 @@ const React = require('react');
 function ListTrue() {
   return (
     <div className="col-md-4 col-sm-12 mt-3">
+      <h6>Your devoured burgers</h6>
       <ul class="list-group" id="eaten">
         {/* <li class="list-group-item">Cras justo odio</li>  */}
       </ul>
